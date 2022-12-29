34df484865305c5a1f7e1eb8b3b8756b0e0a23ab
Yorman Eduardo Amaro Carrero <yormaneduardoamarocarrero@yormans-mbp.lan>
2022-12-29 16:15:06 -0300

    Creation of testing branch and modification of array in data.js

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,63 +1,72 @@
 export const product = [
     {
+        id: articulo01,
         name: 'TEAM 9FORTY CAP',
         price: 29.900, 
         url: 'headwear1.png' 
     },
     {
+        id: articulo02,
         name: 'DR SEASONAL 940',
         price: 27.900, 
         url: 'Dr-seasonal-940.png'
     },
     {
+        id: articulo03,
         name: 'GULF CASUAL CLASSIC',
         price: 29.900, 
         url: 'Gulf-casual-classic.png'
     },
     {
+        id: articulo04,
         name: 'USA VERSITY M',
         price: 29.200, 
         url: 'USA-versity-M.png'
     },
     {
+        id: articulo05,
         name: 'USA VERSITY M BLACK',
         price: 29.200, 
         url: 'USA-versity-M-Black.png'
     },
     {
+        id: articulo06,
         name: 'ESSENTIAL CAP',
         price: 23.800, 
         url: 'Essential-cap.png'
-    }
-]; 
-
-export const productTwo = [
-        {
+    },
+    {
+        id: articulo07,
         name: 'ACTIVE DUALBRAND FULL',
         price: 101.900, 
         url: 'Jumpers-uno.png' 
     },
     {
+        id: articulo08,
         name: 'MENS USA VARSITY',
         price: 53.300, 
         url: 'Jumpers-dos.png'
     },
     {
+        id: articulo09,
         name: 'GULF OVERHEAD HOODIE',
         price: 61.701, 
         url: 'Jumpers-tres.png'
     },
     {
+        id: articulo10,
         name: 'ACTIVE DUALBRAND BLACK',
         price: 101.910, 
         url: 'Jumpers-cuatro.png'
     },
     {
+        id: articulo11,
         name: 'PAPAYA HOODED SWEAT',
         price: 44.350, 
         url: 'Jumpers-cinco.png'
     },
     {
+        id: articulo12,
         name: 'MENS TEAM CORE',
         price: 95.110, 
         url: 'Jumpers-seis.png'
